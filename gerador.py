310949b95907d792180a3a60c58aa738b8e2b3a9
Annanda Sousa <annanda.sousa@gmail.com>
2017-02-02 15:27:55 -0200

    Outro exemplo de gerador.

--- gerador.py
+++ gerador.py
@@ -3,7 +3,13 @@ def gen_123():
     yield 2
     yield 3
 
+def gen_novo():
+    print('iniciando')
+    yield 'a'
+    print('continuando')
+    yield 'b'
+    print('finalizando')
 
-g = gen_123()
-for i in g:
-    print(i)
+# g = gen_123()
+# for i in g:
+#     print(i)
