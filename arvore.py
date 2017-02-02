dc6cfdb0bf8ebf4b6e6b6b43c297b6fdb6ff117f
Annanda Sousa <annanda.sousa@gmail.com>
2017-02-02 21:08:39 -0200

    Percorrendo em profundidade uma árvore binária. A árvore é um gerador.

---
+++ arvore.py
@@ -0,0 +1,30 @@
+class No:
+    def __init__(self, valor, esquerdo=None, direito=None):
+        self.valor = valor
+        self.direito = direito
+        self.esquerdo = esquerdo
+
+
+class Arvore:
+    def __init__(self, raiz=None):
+        self.raiz = raiz
+
+    def __iter__(self):
+        no = self.raiz
+        if no:
+            yield no.valor
+            for filho in Arvore(no.esquerdo):
+                yield filho
+            for filho in Arvore(no.direito):
+                yield filho
+
+
+neto_direita = No('d')
+neto_esquerda = No('c')
+filho_esquerdo = No('b', neto_esquerda, neto_direita)
+filho_direita = No('e')
+no_raiz = No('a', filho_esquerdo, filho_direita)
+arvore = Arvore(no_raiz)
+
+for valor in arvore:
+    print(valor)
